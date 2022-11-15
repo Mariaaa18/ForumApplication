d8a237b3999f3282536e104871f4a6642b43efe8
Mariaaa18 <315266@via.dk>
2022-11-15 09:24:05 +0100

    worksss
    dfghn

--- Shared/Post.cs
+++ Shared/Post.cs
@@ -3,9 +3,9 @@
 public class Post
 {
     public int Id { get; set; }
-    public User Owner { get; private set; }
-    public string Title { get; private set; }
-    public string Body { get; private set; }
+    public User Owner { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
 
     public Post(User owner,string title, string body)
     {
